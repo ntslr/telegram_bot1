91bc57434389e61356455182bc10804cad7e99aa
ntslr <kseniashreider7@inbox.ru>
2026-04-17 20:24:46 +0300

    Update run.py

--- run.py
+++ run.py
@@ -8,11 +8,13 @@ print("🚀 ЗАПУСК ПРИЛОЖЕНИЯ", flush=True)
 
 def run_bot():
     print("🔄 Запуск Telegram-бота...", flush=True)
+    time.sleep(5)
     result = subprocess.run([sys.executable, "bot/bot_main.py"])
     print(f"❌ Бот завершил работу с кодом {result.returncode}", flush=True)
 
 def run_fastapi():
     print("🔄 Запуск FastAPI (Mini App)...", flush=True)
+    # КРИТИЧНО: хост ДОЛЖЕН быть 0.0.0.0
     subprocess.run([sys.executable, "-m", "uvicorn", "webapp.main:app", "--host", "0.0.0.0", "--port", "8000"])
 
 if __name__ == "__main__":
